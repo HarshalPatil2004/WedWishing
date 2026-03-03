8714fdba683036fc07c3a315523fb64034255b77
HarshalPatil2004 <harshalshard.patil2004@gmail.com>
2026-03-03 15:55:00 +0530

    Fix mongodb build crash

--- lib/mongodb.tsx
+++ lib/mongodb.tsx
@@ -1,10 +1,7 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import mongoose from "mongoose";
 
-const MONGO_URI = process.env.MONGO_URI!;
-
-if (!MONGO_URI) {
-  throw new Error("MONGO_URI missing");
-}
+const MONGO_URI = process.env.MONGO_URI;
 
 let cached = (global as any).mongoose;
 
@@ -13,6 +10,10 @@ if (!cached) {
 }
 
 export async function connectDB() {
+  if (!MONGO_URI) {
+    throw new Error("MONGO_URI missing");
+  }
+
   if (cached.conn) return cached.conn;
 
   if (!cached.promise) {
